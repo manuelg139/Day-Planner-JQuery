15fc49054197518a95f399563fdb3837c0015ba6
ManuelGF <manuelg139@gmail.com>
2021-01-26 21:26:27 -0600

    Added the currentTimefucntion

--- assets/script.js
+++ assets/script.js
@@ -1,4 +1,22 @@
 
 // Creating html file with divs for columns
 // set up the time, text imput and save button for day planner
-// 
+// disect css file and match existing classes with our HTML 
+// give each element an id to be able to call in js file 
+// Set up Timer using moment.js
+//Set up local storage 
+
+
+
+
+
+$(document).ready(function() {
+
+
+//Moment.js date for top of page
+function currentTime(){
+    $("#currentDay").text(moment().format('MMMM Do YYYY, h:mm:ss a'));
+    };
+    setInterval(currentTime, 1000);
+})
+
